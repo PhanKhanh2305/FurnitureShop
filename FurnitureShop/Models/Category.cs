18e2ba928b1891ba53d7f8b506e73d8f8bf231bd
DuyKhanh2305 <phanduymanh2305@gmail.com>
2024-08-22 23:41:31 +0700

    Update Template for FurnitureShop

---
+++ FurnitureShop/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FurnitureShop.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        [Required]
+        [StringLength(200)]
+        public string? CategoryName { get; set; }
+        [Required]
+        [StringLength(300)]
+        public string? CategoryPhoto { get; set; }
+        public int CategoryOrder { get; set; }
+    }
+}
---
+++ FurnitureShop/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FurnitureShop.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [Required]
+        [StringLength(200)]
+        public string? ProductName { get; set; }
+        [ForeignKey("Category")]
+        public int CategoryId { get; set; }
+        [Required]
+        [Column(TypeName ="decimal(8,3)")]
+        public decimal? ProductPrice { get; set; }
+        [Required]
+        [StringLength(300)]
+        public string? ProductPhoto { get; set; }
+        public Category? Category { get; set; }
+    }
+}
